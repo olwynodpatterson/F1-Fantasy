c0cd7caca36ab606f042637e743579a36eb28131
Olwyn <odonogol@tcd.ie>
2023-08-29 15:54:09 +0100

    importing data

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+df_drivers = pd.read_csv('datasets/drivers.csv')
+print(df_drivers)
+
+df_races = pd.read_csv('datasets/races.csv')
+print(df_races)
+
+df_driverStandings = pd.read_csv('datasets/driver_standings.csv')
+print(df_driverStandings)
+
+currentDrivers = ('alonso', 'max_verstappen', 'piastri', 'norris', 'leclerc', 'sainz', 'hamilton', 'russell',
+                  'perez', 'ocon', 'gasly', 'tsunoda', 'ricciardo', 'stroll', 'albon', 'sargeant', 'bottas',
+                  'zhou', 'kevin_magnussen', 'hulkenberg')
+
+print(currentDrivers)
+
+df_drivers.loc[:,"driverRef"]
